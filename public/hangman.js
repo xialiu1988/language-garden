27ca04ba484e5c7b9318d6f08a5d18b4e13bb564
Deziree Teague <dez.teague@gmail.com>
2018-12-19 16:59:31 -0800

    hangman duplication in progress

--- public/hangman.js
+++ public/hangman.js
@@ -1,5 +1,23 @@
 'use strict';
 
+//based on example from https://codepen.io/carsy/pen/VvqJwm
+
+let countdownNumberEl = document.getElementById('countdown-number');
+let countdown = 60;
+
+countdownNumberEl.textContent = countdown;
+
+setInterval(function() {
+  countdown = --countdown <= 0 ? 60 : countdown;
+  if (countdown > 60) {
+    alert ('sorry you are out of time')
+  }
+
+  countdownNumberEl.textContent = countdown;
+}, 1000);
+
+//based on demo from https://www.youtube.com/watch?v=f5BbzXgvi1o
+
 const greetingsArray = ['hola','jambo','salaam','bonjour','ohayo','ola','konichiwa'];
 //generate random greeting
 let randomized = Math.floor(Math.random() * greetingsArray.length);
@@ -12,7 +30,6 @@ let underscore = [];
 let underscoreFill = document.getElementsByClassName('underscore');
 let correctletters = document.getElementsByClassName('correctletters');
 let incorrectletters = document.getElementsByClassName('incorrectletters');
-
 
 //this function generates the correct number of underscores based on the length of the selected word
 let setupUnderscores = () => {
