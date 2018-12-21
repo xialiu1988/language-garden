327328920b813fe6d6af881b434e92509c66fefe
shalom belaineh <shalom.belaineh@gmail.com>
2018-12-20 17:50:05 -0800

    signupcss

--- public/dialogue.js
+++ public/dialogue.js
@@ -30,30 +30,29 @@ $(document).ready(function () {
   });
 });
 
-  $('.form').on('click', function(){
-    console.log('I\'m alive');
+$('.form').on('click', function(){
+  console.log('I\'m alive');
 
 
-    var $toggle=$(this);
-    var textid='#textq-'+$toggle.data('id');
-    var tarid='#dialogue-'+$toggle.data('id');
-    console.log(textid+'hey');
-    console.log(tarid);
-    console.log($(textid).val());
-    console.log($(tarid).val());
+  var $toggle=$(this);
+  var textid='#textq-'+$toggle.data('id');
+  var tarid='#dialogue-'+$toggle.data('id');
+  console.log(textid+'hey');
+  console.log(tarid);
+  console.log($(textid).val());
+  console.log($(tarid).val());
 
-    var url = 'https://translation.googleapis.com/language/translate/v2?key=AIzaSyCu_L955TWKLiDJdqI61-3xf5M_Ankyh44';
-    url += '&source=' + 'en';
-    url += '&target=' + $(tarid).val();
-    url += '&q=' + escape($(textid).val());
+  var url = 'https://translation.googleapis.com/language/translate/v2?key=AIzaSyCu_L955TWKLiDJdqI61-3xf5M_Ankyh44';
+  url += '&source=' + 'en';
+  url += '&target=' + $(tarid).val();
+  url += '&q=' + escape($(textid).val());
 
-    $.get(url, function (data, status) {
-      $('#text-'+$toggle.data('id')).val(data.data.translations[0].translatedText);
-    });
-
-
+  $.get(url, function (data, status) {
+    $('#text-'+$toggle.data('id')).val(data.data.translations[0].translatedText);
   });
 
+
 });
+
 
 
